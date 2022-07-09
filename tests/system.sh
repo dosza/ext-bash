ccb4e957b5a116d15d0cb2b3526996a98647f95a
dosza <oliveira.daniel109@gmail.com>
2022-07-09 17:06:18 -0300

    add tests to isVariable family functions

--- tests/system.sh
+++ tests/system.sh
@@ -44,6 +44,55 @@ testIsFileBusy(){
 	assertEquals "[Running IsFileBusy with file busy (locked) ]" "Wait for bash..." "$(IsFileBusy bash $tmp_busy_file)"
 	unlockFile
 	rm $tmp_busy_file
+}
+
+testIsVariableArray(){
+	local array=()
+	local fake_array='a=()'
+	local -A hash_array=()
+
+	isVariableArray array
+	assertTrue '[Test array as success]' $?
+
+	isVariableArray fake_array
+	assertFalse '[Test if a variable type string is array]' $?
+
+	isVariableArray ""
+	assertFalse '[Test if variable is array with few args]' $?
+
+	isVariableArray hash_array
+	assertTrue '[Test  associative array as success]' $?	
+}
+testIsVariableAssociativeArray(){
+	local array=()
+	local fake_array='a=()'
+	local -A hash_array=()
+
+	isVariableAssociativeArray fake_array
+	assertFalse '[Test if a variable type string is  associative array]' $?
+
+	isVariableAssociativeArray ""
+	assertFalse '[Test if variable is array with few args]' $?
+
+	isVariableAssociativeArray hash_array
+	assertTrue '[Test  associative array as success]' $?	
+}
+
+testIsVariabelDeclared(){
+	G_VARIABLE=''
+	local local_var=''
+
+	isVariabelDeclared G_VARIABLE
+	assertTrue '[Test as sucessful variable declared]' $?
+
+	unset G_VARIABLE
+	isVariabelDeclared G_VARIABLE
+	assertFalse '[Test a non existent variable]' $?
+
+	isVariabelDeclared local_var
+	assertTrue '[Test as sucessful local variable declared]' $?
 
 }
+
+
 . $(which shunit2)
